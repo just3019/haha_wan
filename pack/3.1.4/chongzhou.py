69c6ec0789781a793f8ddd62e14bf3bf929a7408
songlinhao <455674848@qq.com>
2018-08-02 13:57:17 +0800

    添加崇州。

--- pack/3.1.4/chongzhou.py
+++ pack/3.1.4/chongzhou.py
@@ -16,12 +16,12 @@ headers = {
     'Accept-Language': 'zh-cn',
 }
 header_dict = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko'}
-TOKEN = '00499849cbf687835af75182698438eb3c2ccdf4'
-plazaId = '1000769'
+TOKEN = '007373010e787df886c6a246f5877fec161a9316'
+plazaId = '1104507'
 ITEMID = '7982'
 count = 0
 province = '330000'
-place = '平阳'
+place = '崇州'
 lock = threading.Lock()
 
 
