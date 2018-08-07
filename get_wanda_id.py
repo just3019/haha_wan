ae3e765df20ff72e8e9b8b72d57764964e406b4f
songlinhao <455674848@qq.com>
2018-08-07 23:05:51 +0800

    拉取万达广场id，添加衢州。

--- get_wanda_id.py
+++ get_wanda_id.py
@@ -1,17 +1,4 @@
-import requests
-
-headers = {
-    'Host': 'api.ffan.com',
-    'Content-Type': 'application/json',
-    'Accept': '*/*',
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15F79 MicroMessenger/6.7.1 NetType/WIFI Language/zh_CN',
-    'Referer': 'https://servicewechat.com/wx07dfb5d79541eca9/83/page-frame.html',
-    'Accept-Language': 'zh-cn',
-}
-
-response = requests.get('https://api.ffan.com/wechatxmt/v1/cities', headers=headers)
-print(response.text)
-
+import json
 
 import requests
 
@@ -24,4 +11,33 @@ headers = {
     'Accept-Language': 'zh-cn',
 }
 
-response = requests.get('https://api.ffan.com/wechatxmt/v1/cities', headers=headers)
+
+def get_park(cityId):
+    params = (
+        ('cityId', cityId),
+    )
+
+    response = requests.get('https://api.ffan.com/wechatxmt/v1/plazas', headers=headers, params=params)
+    print(response.text)
+    return response.text
+
+
+def get_cities():
+    response = requests.get('https://api.ffan.com/wechatxmt/v1/cities', headers=headers)
+    print("==" + response.text)
+    return response.text
+
+
+if __name__ == '__main__':
+
+    f = open("广场id.txt", "w")
+
+    cities = json.loads(get_cities())
+    for c in cities['data']:
+        park = json.loads(get_park(c['cityId']))
+        for pp in park['data']:
+            p = str(pp['plazaId']) + " " + pp['plazaName'] + " " + str(pp['longitude']) + " " + str(pp['latitude'])
+            print(p)
+            f.write('%s\n' % p)
+
+    f.close()
