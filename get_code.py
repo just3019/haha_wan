20c713c23bfb937b2d73c565ad87c0f8eb30bfd9
songlinhao <455674848@qq.com>
2018-08-14 20:04:31 +0800

    重写业务代码。

--- get_code.py
+++ get_code.py
@@ -6,26 +6,13 @@ import requests
 
 # 获取验证码
 def get_code(mobile):
-    import requests
-
-    headers = {
-        'Host': 'api.ffan.com',
-        'Accept': '*/*',
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15G77 MicroMessenger/6.7.1 NetType/4G Language/zh_CN',
-        'Referer': 'https://servicewechat.com/wx07dfb5d79541eca9/83/page-frame.html',
-        'Accept-Language': 'zh-cn',
-    }
-
     params = (
         ('sign', 'xcx'),
     )
-
     data = [
         ('mobile', mobile),
     ]
-
-    response = requests.post('https://api.ffan.com/wechatxmt/v1/member/verifyCode', headers=headers, params=params,
-                             data=data)
+    response = requests.post('https://api.ffan.com/wechatxmt/v1/member/verifyCode', params=params, data=data)
     print('获取验证码接口返回参数：\n' + response.text)
 
 
---
+++ package_new.py
@@ -0,0 +1,7 @@
+import threading
+from tkinter import *
+from urllib import request
+import time
+import json
+import re
+import requests
