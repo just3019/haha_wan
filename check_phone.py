a06ed0b930b64cdd1aa5b319ff1e4b27b1293908
songlinhao <455674848@qq.com>
2018-08-28 23:44:41 +0800

    修改检查号码

--- check_phone.py
+++ check_phone.py
@@ -10,10 +10,20 @@ headers = {
     'orgTypeCode': '10003',
     'token': 'MjM1MDY0MDgxMjgxNzEyMTI4',
 }
+file = "滨州新人礼20180827"
+file_write = "/Users/demon/Desktop/1/" + file + "校验号码.txt"
+file_read = "/Users/demon/Desktop/1/" + file + ".txt"
+name = "滨州万达广场"
+# 非该广场名
+notequal = ""
+# 非该广场数
+notequal_num = 0
+# 查询不到数
+no_num = 0
 
 
 def write(s):
-    f = open("校验号码.txt", "a")
+    f = open(file_write, "a")
     f.write('%s\n' % s)
     f.close()
 
@@ -33,12 +43,14 @@ def check(phone):
     response = requests.get('http://wanda.ffan.com/sail/member/list', headers=headers, params=params)
     print(response.text)
     result = json.loads(response.text)
-    if result["status"] == "0000":
+    if result["status"] == "0000" and len(result["data"]) > 0:
         return result["data"][0]
+    else:
+        raise RuntimeError("查询不到")
 
 
 if __name__ == '__main__':
-    file = open("/Users/demon/PycharmProjects/wanda/v3/台州20180828.txt", 'r')
+    file = open(file_read, 'r')
     index = 0
     t1 = time.time()
     while True:
@@ -48,12 +60,21 @@ if __name__ == '__main__':
             if not mystr:
                 break
             phone = mystr[0: 11]
-            print(phone)
+            print(index)
             result = check(phone)
+            address = result["fromOrg"]
+            if address != name:
+                if address not in notequal:
+                    notequal += " " + address
+                notequal_num += 1
             write(phone + " " + result["fromOrg"])
         except RuntimeError as e:
+            no_num += 1
             print(e)
+            write(phone + " 查询不到")
             continue
+    p = "总共：" + str(index - 1) + " 非该广场数：" + str(notequal_num) + " 查询不到广场数：" + str(no_num) + "\n非该广场名：" + str(notequal)
+    write(p)
     t2 = time.time()
     print("总共使用：" + str(t2 - t1))
     file.close()
