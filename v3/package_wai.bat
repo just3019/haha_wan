364ae35f40144edf23e253e57725043ca4ccf599
songlinhao <455674848@qq.com>
2018-09-12 21:43:35 +0800

    update

---
+++ v3/package_wai.bat
@@ -0,0 +1,8 @@
+pyinstaller -F beijinghuaifang_wai.py
+pyinstaller -F kunmingxishan_wai.py
+pyinstaller -F pingyang_wai.py
+pyinstaller -F qingdaolicang_wai.py
+pyinstaller -F sanming_wai.py
+pyinstaller -F taizhou_wai.py
+pyinstaller -F tongliao_wai.py
+pyinstaller -F wuhai_wai.py
