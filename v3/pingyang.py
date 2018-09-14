37c8ff8add3e9400a666a3d6891f41d6283af0f9
songlinhao <455674848@qq.com>
2018-09-14 09:32:53 +0800

    update

--- v3/pingyang.py
+++ v3/pingyang.py
@@ -2,10 +2,10 @@ import wanda_utils
 
 TOKEN = ''
 PLAZAID = '1000769'
-PROVINCE = '330000'
+PROVINCE = ''
 PLACE = '平阳'
-XM_LOCAL = "浙江"
-HM_PROVINCE = "浙江"
+XM_LOCAL = ""
+HM_PROVINCE = ""
 
 if __name__ == '__main__':
     wanda_utils.init(PLACE, TOKEN, PROVINCE, PLAZAID, HM_PROVINCE, XM_LOCAL)
