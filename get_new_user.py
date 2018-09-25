338d4a1345a96acd3062207b61955d65871648e3
songlinhao <455674848@qq.com>
2018-09-25 20:04:38 +0800

    update

--- get_new_user.py
+++ get_new_user.py
@@ -133,7 +133,7 @@ def get_all_guangchang(start, end):
         result = mystr.split(" ")
         print(result)
         org_id = result[0]
-        place_name = "920/" + result[1] + ".txt"
+        place_name = "925/" + result[1] + ".txt"
         file_write = open(place_name, "a")
         total_count = 0
         init()
@@ -206,7 +206,7 @@ def get_no_done_user_count(start1, end1, start2, end2):
 
 
 def write(s):
-    f = open("广场完成情况4.txt", "a")
+    f = open("广场完成情况5.txt", "a")
     f.write('%s\n' % s.strip())
     f.close()
 
@@ -223,9 +223,9 @@ if __name__ == '__main__':
 
     print("开始main方法")
 
-    # get_no_done_user_count(start1, end1, start2, end2)
+    get_no_done_user_count(start1, end1, start2, end2)
     # get_all_guangchang(start1, end1)
-    get_one_guangchang("1104805", "大连甘井子万达广场", "大连", start1, end1)
+    # get_one_guangchang("1000644", "赤峰万达广场", "赤峰", start1, end1)
     # local_file = "/Users/demon/PycharmProjects/wanda/v3song/丹东新人礼20180911-check.txt"
     # write_file = "/Users/demon/PycharmProjects/wanda/v3song/丹东新人礼20180911-check-result.txt"
     # get_and_check_local_phone(local_file, write_file)
