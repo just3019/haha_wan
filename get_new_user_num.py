d9aa6e05c9e0aad01e1b37fee10d42e3d7a016d9
songlinhao <455674848@qq.com>
2018-09-29 23:08:07 +0800

    update

--- get_new_user_num.py
+++ get_new_user_num.py
@@ -2,7 +2,7 @@ import requests
 
 cookies = {
     'sensorsdata2015jssdkcross': '%7B%22distinct_id%22%3A%22adminkmxs%22%2C%22%24device_id%22%3A%22164c642c5b7407-048ae012493e02-163b6952-1296000-164c642c5b837%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%2C%22first_id%22%3A%22164c642c5b7407-048ae012493e02-163b6952-1296000-164c642c5b837%22%7D',
-    'JSESSIONID': '0A99EBD1A8A7824E10C3C518F6811F2A',
+    'JSESSIONID': '8731C48BFE9B4B40F0E870DE4A93F56D',
 }
 
 headers = {
@@ -25,16 +25,19 @@ headers = {
 }
 
 params = (
-    ('scope', '1000625'),
-    ('orgType', '10003'),
+    ('date/[/]', ['2018-09-28', '2018-09-28']),
+    ('orgCode', '1000625'),
+    ('channel', '01'),
     ('startDate', '2018-09-29'),
     ('endDate', '2018-09-29'),
+    ('timeType', 'day'),
+    ('pageSize', '10'),
     ('pageIndex', '1'),
-    ('pageSize', '1'),
-    ('timestr', '1538180602772'),
+    ('orgType', '10003'),
+    ('timeOrder', 'descending'),
+    ('selectOrg/[/]', '1000625'),
+    ('timestr', '1538186155447'),
 )
 
-response = requests.get(
-    'http://wanda.ffan.com/sail/member/report/analyse/expandingAnalyse/expandingSmallProcedureChannel', headers=headers,
-    params=params, cookies=cookies)
+response = requests.get('http://wanda.ffan.com/sail/member/report/analyse/memberNumByPlaza', headers=headers, params=params, cookies=cookies)
 print(response.text)
