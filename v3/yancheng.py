3695d91ec69e47ff96c9c004a4858aa73a175823
songlinhao <455674848@qq.com>
2018-10-12 14:02:28 +0800

    update

---
+++ v3/yancheng.py
@@ -0,0 +1,12 @@
+import wanda_utils
+
+TOKEN = ''
+PLAZAID = '1104112'
+PROVINCE = ''
+PLACE = '盐城'
+XM_LOCAL = ""
+HM_PROVINCE = ""
+
+if __name__ == '__main__':
+    wanda_utils.init(PLACE, TOKEN, PROVINCE, PLAZAID, HM_PROVINCE, XM_LOCAL)
+    wanda_utils.ui()
