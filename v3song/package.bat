0e81488a306c6432e32148c9eb08628f5bc3cbc5
songlinhao <455674848@qq.com>
2018-11-10 08:44:18 +0800

    update

---
+++ v3song/package.bat
@@ -0,0 +1,7 @@
+pyinstaller -F beijingfengtai.py
+pyinstaller -F chaoyang.py
+pyinstaller -F daliankaifa.py
+pyinstaller -F dalianzhuanghe.py
+pyinstaller -F dandong.py
+pyinstaller -F manzhouli.py
+pyinstaller -F yingkou.py
