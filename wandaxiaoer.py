3c7e6d4931eed7ad85bf38f82089ef0f7c5daffd
songlinhao <455674848@qq.com>
2018-11-15 10:12:58 +0800

    update

--- wandaxiaoer.py
+++ wandaxiaoer.py
@@ -7,6 +7,10 @@ from tkinter import filedialog
 
 import requests
 
+from thread_pool import ThreadPool
+
+lock = threading.Lock()
+
 
 def printf(s):
     print('[%s][%s]%s' % (threading.current_thread().getName(), time.strftime("%X"), s))
@@ -83,9 +87,69 @@ def ui():
 
     fm4 = Frame(root)
     fm4.pack()
-    Button(fm4, text="核销").pack(side=LEFT)
+    Button(fm4, text="核销", command=verification).pack(side=LEFT)
 
     root.mainloop()
+
+
+def verification():
+    th = threading.Thread(target=deal)
+    th.setDaemon(True)
+    th.start()
+
+
+def deal():
+    lock.acquire()
+    file = open(file_path, 'r')
+    index = 0
+    t1 = time.time()
+    TP = ThreadPool(30)
+    while True:
+        try:
+            index += 1
+            mystr = file.readline()
+            if not mystr:
+                break
+            code = mystr.split("|")[1]
+            TP.add_task(scan, code, index)
+            time.sleep(get_interval_time())
+        except RuntimeError as e:
+            log(e)
+            continue
+    TP.wait_completion()
+    log("总共使用：%s" % (time.time() - t1))
+    file.close()
+    lock.release()
+
+
+def scan(code, index):
+    try:
+        log("第%s个核销开始" % index)
+        # verify(code)
+
+        log("核销第%s个券%s成功" % (index, code))
+    except RuntimeError as e:
+        log("%s核销有问题" % code)
+
+
+def verify(code):
+    storeId = user["orgList"][0]["code"]
+    tenantid = user["orgList"][0]["tenantId"]
+    userid = user["id"]
+    token = user["token"]
+    headers = {
+        'charset': 'utf-8',
+        'referer': 'https://servicewechat.com/wx57ebde74de5e6267/6/page-frame.html',
+        'workingorgcode': storeId,
+        'tenantid': tenantid,
+        'token': token,
+        'userid': userid,
+        'content-type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; MP1503 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36 MicroMessenger/6.7.3.1360(0x26070339) NetType/WIFI Language/zh_CN Process/appbrand2',
+        'Host': 'api.beyonds.com',
+    }
+    data = 'couponCode=%s&storeId=%s' % (code, storeId)
+    response = requests.post('https://api.beyonds.com/wpxe/v1/coupon/verify', headers=headers, data=data)
 
 
 def send_v_code():
@@ -109,6 +173,23 @@ def send_v_code():
 
 def login():
     printf("登录")
+    phone = entryPhone.get()
+    code = entryCode.get()
+    headers = {
+        'charset': 'utf-8',
+        'referer': 'https://servicewechat.com/wx57ebde74de5e6267/6/page-frame.html',
+        'content-type': 'application/x-www-form-urlencoded',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; MP1503 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36 MicroMessenger/6.7.3.1360(0x26070339) NetType/WIFI Language/zh_CN Process/appbrand2',
+        'Host': 'api.beyonds.com',
+    }
+    data = 'type=2&mobile=%s&verifyCode=%s' % (phone, code)
+    response = requests.post('https://api.beyonds.com/wpxe/v1/user/login', headers=headers, data=data)
+    printf(response.text)
+    result = json.loads(response.text)
+    if result["status"] == 200:
+        global user
+        user = result["data"]
+        log("%s登录成功" % phone)
 
 
 if __name__ == '__main__':
